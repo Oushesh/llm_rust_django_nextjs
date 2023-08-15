f3be3338cbe3f77f4f83c742cbfcb801bf5c0fe3
oushesh <oushesh@gmail.com>
2023-08-15 12:37:35 +0400

    updated main.go

--- manager/main.go
+++ manager/main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Article struct {
-	Title   string "json:Title"
-	Desc    string "json:"
-	Content string "json:content"
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
 }
+
 type Articles []Article
 
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
+	}
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
 func handlePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Homepage Endpoint hit") // Use fmt.Fprint instead of fmt.Fprintf
+	fmt.Fprint(w, "Homepage Endpoint hit")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", handlePage) // Use the correct function name
-	//log.Fatal(http.ListenAndServeTLS(":8081", "cert.pem", "key.pem", nil)) // Provide the certificate and key files
-	log.Fatal(http.ListenAndServe(":8081", nil)) // Provide the certificate and key files
+	http.HandleFunc("/", handlePage)
+	http.HandleFunc("/articles", allArticles) // Add the route for /articles
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func main() {

f1c9310e8b7c581250205004cc4fdff05945e10a
oushesh <oushesh@gmail.com>
2023-08-26 22:11:10 +0400

    successful gpt_stream

--- Backend/hallucination_app/routers/gpt_stream.py
+++ Backend/hallucination_app/routers/gpt_stream.py
@@ -7,21 +7,26 @@ router = Router()
 
 # Get the absolute path of the current script
 current_script_dir = os.path.dirname(os.path.abspath(__file__))
-
 # Calculate the project root directory (three levels up from loaders.py)
-project_root = os.path.abspath(
-    os.path.join(current_script_dir, "../../../../rust_core/ggml")
-)
+project_root = os.path.abspath(os.path.join(current_script_dir, "../../.."))
+
+print("project_root stream", project_root)
 
 
 # The subprocess command
 @router.get("/stream")
 def generate_output(request):
     def stream_output():
+        project_root = os.path.abspath(os.path.join(current_script_dir, "../../.."))
         command = [
-            ".project_root/build/bin/gpt-2",
+            os.path.join(
+                ".", os.path.join(project_root, "rust_core/ggml/build/bin/gpt-2")
+            ),
             "-m",
-            "project_root/models/gpt-2-117M/ggml-model-gpt-2-1558M.bin",
+            os.path.join(
+                project_root,
+                "rust_core/ggml/models/gpt-2-117M/ggml-model-gpt-2-1558M.bin",
+            ),
             "-p",
             "What is the capital of France?",
         ]

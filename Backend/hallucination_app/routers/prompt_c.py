ca0c21065b30f6ecdda1c5d572a4e88f56298504
oushesh <oushesh@gmail.com>
2023-09-12 15:46:06 +0400

    added prompt_c_version.py

--- Backend/hallucination_app/routers/prompt_c.py
+++ Backend/hallucination_app/routers/prompt_c.py
@@ -13,20 +13,21 @@ import subprocess
 router = Router()
 
 #TODO: update to add the argument for model_path
+from django.http import StreamingHttpResponse
+
 @router.get("/prompt_c")
-def AIChat(request,input:str):
+def StoryTelling(request,input:str):
+    assert (isinstance(input,str))
     llama2_bin = "/Users/ousheshharadhun/Documents/Workspace/FacebookLLAMA/LLM_Backup/llm_backend_go/Backend/manager/llama2.c/./run"
     llama2_model = "/Users/ousheshharadhun/Documents/Workspace/FacebookLLAMA/LLM_Backup/llm_backend_go/Backend/manager/llama2.c/stories15M.bin"
 
     cmd = [llama2_bin, llama2_model, "-t", "0.8", "-n", "256", "-i", input]
     process = subprocess.Popen(cmd,stdout=subprocess.PIPE,text=True)
 
-    output = []
-    for line in iter(process.stdout.readline,""):
-        output.append(line)
-        #Here, handling of each line of output in real-time if needed
-        print (line,end="")
+    def stream():
+        for line in iter(process.stdout.readline, ""):
+            yield f"data: {line}\n\n"
+        process.stdout.close()
+        process.wait()
 
-    process.stdout.close()
-    process.wait()
-    return "".join(output)
+    return StreamingHttpResponse(stream(), content_type="text/event-stream")
